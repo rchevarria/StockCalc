30fe378ca753b956962589ad4daa0067106b4946
Ryan Chevarria <Ryan.Chevarria64@myhunter.cuny.edu>
2024-11-27 14:26:51 -0500

    Initial commit for StockCalcproject

---
+++ StockCalc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockCalc
+//
+//  Created by Ryan Chevarria on 11/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
